b6857576c2b473923ddcbd6be69ebbcc1b5a1926
Matthew Lauriault <mlauriault03@gmail.com>
2025-11-03 14:17:29 -0500

    running tests with docker

---
+++ codrone_test/codrone_test/test_codrone.py
@@ -0,0 +1,15 @@
+from codrone_edu.drone import Drone
+import time
+
+print("Connecting to CoDrone...")
+d = Drone()
+d.pair()
+print("Connected!")
+
+d.takeoff()
+d.hover(1)
+d.land()
+d.close()
+
+print("Done.")
+
