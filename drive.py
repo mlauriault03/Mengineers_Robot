57773c733752f340ece360f4b58de8d26b6677e7
Matthew <mlauriault03@gmail.com>
2025-12-10 21:31:03 -0500

    Add direction PID to move_forward

--- drive.py
+++ drive.py
@@ -8,6 +8,7 @@ import math
 
 
 # PRIVATE LIBRARIES
+from pid import PID
 from encoder import Encoder
 from servo import Servo
 
@@ -48,6 +49,11 @@ class Drive:
     RIGHT_PULSE_NEUTRAL = 1500    # 1500 [ms] stop
     RIGHT_PULSE_FORWARD = 2000    # 2000 [ms] full speed forward
 
+    # PID PARAMETERS
+    DIR_KP = 0.05
+    DIR_KI = 0.00
+    DIR_KD = 0.00
+
 
     # CONSTRUCTOR
 
@@ -65,6 +71,9 @@ class Drive:
         # )
         self.encoder_left = Encoder(addr_encoder_left, 1)
         self.encoder_right = Encoder(addr_encoder_right, -1)
+
+        # Direction PID controller
+        self.dir_pid = PID(self.DIR_KP, self.DIR_KI, self.DIR_KD, -0.2, 0.2)
 
 
     # UTILITY METHODS
@@ -85,7 +94,7 @@ class Drive:
         self.servo_right.set_speed(right)
         self.servo_left.set_speed(left)
 
-    def _start_servos(self):
+    def _startup_servos(self):
         self.servo_right.startup()
         self.servo_left.startup()
 
@@ -95,9 +104,10 @@ class Drive:
         self.servo_left.stop()
         time.sleep(1)
 
-    def _shutdown(self):
+    def _shutdown_servos(self):
         self.servo_right.shutdown()
         self.servo_left.shutdown()
+        time.sleep(1)
 
     def _ramp_up(self, end_speed: int, steps: int = 20):
         for i in range(0, steps + 1):
@@ -105,17 +115,55 @@ class Drive:
             self._set_speed(speed, speed)
             time.sleep(0.05)
 
-    
-    
+    def _get_position(self) -> tuple[int, int]:
+        return (self.encoder_left.get_position(), self.encoder_right.get_position())
+
 
     # PUBLIC METHODS
 
     def move_forward(self, distance_in: int):
         """Move forward specified distance in inches"""
-        self._start_servos()
-        ticks = distance_in # TESTING ONLY
-        # self._ramp_up(1)
-        self._set_speed(0.5, 0.5)
-        time.sleep(5)
-        self._shutdown()
-        # Set target position for both wheels (same absolute value)
+        self._startup_servos()
+        self.dir_pid.reset()
+        target_ticks = self._distance_to_ticks(distance_in)
+        # servo speed variables
+        left_speed = 1
+        right_speed = 1
+        # control loop
+        while True:
+            # determine left and right wheel position difference
+            left_ticks, right_ticks = self._get_position()
+            tick_diff = left_ticks - right_ticks
+            # compute left servo speed compensation
+            left_comp = self.dir_pid.update(target=0, current=tick_diff)
+            # stop either servo if they have reached the target position
+            if abs(left_ticks - target_ticks) <= 1:
+                self.servo_left.shutdown() # left servo reached target
+                left_speed = None
+            if abs(right_ticks - target_ticks) <= 1:
+                self.servo_right.shutdown() # right servo reached target
+                right_speed = None
+            # break condition
+            if (left_speed is None) and (right_speed is None): 
+                break
+            # compensate left and right speeds if both servos are still running
+            if (left_speed is not None) and (right_speed is not None):
+                left_speed = left_speed + left_comp
+                right_speed = right_speed - left_comp
+            # update left servo speed (if still running)
+            if left_speed is not None:
+                self.servo_left.set_speed(left_speed)
+            # update right servo speed (if still running)
+            if right_speed is not None:
+                self.servo_left.set_speed(right_speed)
+            time.sleep(0.05) # 20 Hz control loop
+        time.sleep(1) # allow time for servo threads to join main thread
+
+        # ---------------------------- TEST CODE ----------------------------
+        # self._start_servos()
+        # ticks = distance_in # TESTING ONLY
+        # # self._ramp_up(1)
+        # self._set_speed(0.5, 0.5)
+        # time.sleep(5)
+        # self._shutdown()
+        # -------------------------------------------------------------------
